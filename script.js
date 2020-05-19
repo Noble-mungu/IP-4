99169c5ee7f7970bb2eb7ea494163c96d3de4440
Noble Mungu <mungunoble@gmail.com>
2020-05-19 10:14:27 +0300

    functions that calculates  price of sizes

--- script.js
+++ script.js
@@ -63,3 +63,37 @@ $(document).ready(function(){
          console.log(total);
          let checkoutTotal =0;
          checkoutTotal = checkoutTotal + total;
+         $("#pizzaname").html($(".name option:selected").val());
+   $("#pizzasize").html( $("#size option:selected").val());
+   $("#pizzacrust").html($("#crust option:selected").val());
+   $("#pizzatopping").html(ptopping.join(", "));
+   $("#totals").html(total);
+
+// Add pizza button
+   $("button.addPizza").click(function(){
+     let pname = $(".name option:selected").val();
+     let psize = $("#size option:selected").val();
+     let pcrust = $("#crust option:selected").val();
+     let ptopping = [];
+     $.each($("input[name='toppings']:checked"), function(){
+         ptopping.push($(this).val());
+     });
+     console.log(ptopping.join(", "));
+     switch(psize){
+       case "0":
+         price =0;
+       break;
+       case "large":
+          price = 1200;
+          console.log(price);
+        break;
+        case "medium":
+          price = 850;
+          console.log("The price is "+price);
+        break;
+        case "small":
+          price = 600;
+          console.log(price);
+        default:
+          console.log("error");
+      }
