d4b2c4768e2333ff0b2ec315324d5f7035866283
Noble Mungu <mungunoble@gmail.com>
2020-05-26 12:12:08 +0300

    add variable name,size,crust,topping,total

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
+}
+
 $(document).ready(function(){
 
   $("button.proceed").click(function(event){
@@ -177,6 +187,6 @@ $(document).ready(function(){
         $("button#final-order").show();
       }
     });
-   
+
   });
 });
